9d5956fca890afdb8bdc48dccdaf2474046b1b6e
Ikam213 <afonin05@yandex.ru>
2024-10-28 04:14:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw1/MyActivity.kt
@@ -0,0 +1,44 @@
+package com.example.hw1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MyActivity : AppCompatActivity() {
+
+    lateinit var recyclerView: RecyclerView
+    lateinit var fab: FloatingActionButton
+
+    private val adapter = MyAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recycler_view)
+        fab = findViewById(R.id.fab)
+
+        recyclerView.adapter = adapter
+
+        fab.setOnClickListener {
+            adapter.addItems(adapter.itemCount + 1)
+        }
+
+        if (savedInstanceState != null){
+            val i=savedInstanceState.getInt("list_size")
+            var j=1
+            while (j<=i){
+               adapter.addItems(j)
+               j+=1
+            }
+        }
+        else {
+            adapter.setItems(listOf(1, 2, 3, 4, 5))
+        }
+}
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("list_size", adapter.itemCount)
+    }
+}
---
+++ app/src/main/java/com/example/hw1/MyViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.hw1
+
+import android.graphics.Color
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val text = view.findViewById<TextView>(R.id.text_view)
+    val image = view.findViewById<ImageView>(R.id.image_view)
+
+    fun bind(number: Int){
+        text.text="$number"
+        if (number%2==1){
+            image.setBackgroundColor(Color.argb(255,0,0, 255))
+        }
+        else{
+            image.setBackgroundColor(Color.argb(255, 255, 0, 0))
+        }
+    }
+}
